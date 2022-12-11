d5dd1efea3b233406329077b5537314c245f28c0
Yibo Zhang <yibo.zhang@pathai.com>
2022-12-10 20:16:50 -0500

    Small touch up

--- problems_2022/day9/solution.py
+++ problems_2022/day9/solution.py
@@ -42,34 +42,45 @@ def _tail_positions(start_position: _Position, instructions: List[_Instruction],
         for _ in range(num_steps):
             # The head position simply follows the instruction.
             head_position = knot_positions[0]
-            if direction == "R":
-                head_position[1] += 1
-            elif direction == "L":
-                head_position[1] -= 1
-            elif direction == "U":
-                # Use convention that going down is +1, and going up is -1.
-                head_position[0] -= 1
-            elif direction == "D":
-                head_position[0] += 1
+            _move_one_step(position=head_position, direction=direction)
 
             # Recursively find the next knot's position based on the previous knot's position.
             for knot_index in range(1, num_knots):
-                previous_knot_position = knot_positions[knot_index - 1]
-                knot_position = knot_positions[knot_index]
-                # Only need to move if in at least one of the axes, the distance is 2.
-                if np.max(np.abs(previous_knot_position - knot_position)) > 1:
-                    axes_with_distance_2 = np.where(np.abs(previous_knot_position - knot_position) == 2)[0]
-                    for axis in range(2):
-                        if axis in axes_with_distance_2:
-                            # Move the current knot 1 step towards the previous knot, which is equivalent to moving the
-                            # current knot to the middle point between the current knot and previous knot.
-                            knot_position[axis] = int((knot_position[axis] + previous_knot_position[axis]) / 2)
-                        else:
-                            knot_position[axis] = previous_knot_position[axis]
+                _update_knot_position(
+                    this_knot_position=knot_positions[knot_index], previous_knot_position=knot_positions[knot_index - 1]
+                )
 
             ret.append(tuple(int(x) for x in knot_positions[-1]))
 
     return ret
+
+
+def _move_one_step(position: np.ndarray, direction: str) -> None:
+    if direction == "R":
+        position[1] += 1
+    elif direction == "L":
+        position[1] -= 1
+    elif direction == "U":
+        # Use convention that going down is +1, and going up is -1.
+        position[0] -= 1
+    elif direction == "D":
+        position[0] += 1
+
+
+def _update_knot_position(this_knot_position: np.ndarray, previous_knot_position: np.ndarray) -> None:
+    """Updates `this_knot_position` in-place, assuming `previous_knot_position` has already been updated."""
+    # Only need to move if in at least one of the axes, the distance is 2.
+    if np.max(np.abs(previous_knot_position - this_knot_position)) <= 1:
+        return
+
+    axes_with_distance_2 = np.where(np.abs(previous_knot_position - this_knot_position) == 2)[0]
+    for axis in range(2):
+        if axis in axes_with_distance_2:
+            # Move the current knot 1 step towards the previous knot, which is equivalent to moving the
+            # current knot to the middle point between the current knot and previous knot.
+            this_knot_position[axis] = int((this_knot_position[axis] + previous_knot_position[axis]) / 2)
+        else:
+            this_knot_position[axis] = previous_knot_position[axis]
 
 
 if __name__ == "__main__":
